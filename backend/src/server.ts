b5b746b17c336febd96428542c2cdfdd1fc7ca4b
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-06-04 00:39:56 -0300

    Configuraçao da conexao com o banco de dados sqlite com knex

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-app.use(express.json);
-
-app.get('/', (request, response) => {
-  return response.json({ ok: 'ok' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
