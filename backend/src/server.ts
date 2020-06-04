d7e459f03956ec410295d9f7cd102832731502df
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-06-04 00:15:46 -0300

    ajuste na configuraçao dos padroes do projeto

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response)=> {
-  return response.json({ok: 'ok'});
-})
+app.get('/', (request, response) => {
+  return response.json({ ok: 'ok' });
+});
 
-app.listen(3333)
+app.listen(3333);
